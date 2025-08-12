668ecdf6129e5d318e471bb30c5369b093a522d0
EGOIST <86555131+EGOIST1372@users.noreply.github.com>
2025-08-13 02:47:44 +0330

    Update BundleTooltipUtil.java

--- src/client/java/io/github/andrew6rant/betterbundletooltips/BundleTooltipUtil.java
+++ src/client/java/io/github/andrew6rant/betterbundletooltips/BundleTooltipUtil.java
@@ -16,10 +16,10 @@ public class BundleTooltipUtil {
 
     public static void drawProgressBar(int x, int y, TextRenderer textRenderer, DrawContext drawContext, Identifier progressBarFillTexture, int progressBarFill, Identifier progressBarBorderTexture) {
     // Draw the filled portion of the progress bar
-    drawContext.drawGuiTexture(progressBarFillTexture, x + 1, y - 2, progressBarFill, 4);
+    drawContext.drawGuiTexture(RenderLayer.getGuiTextured(), progressBarFillTexture, x + 1, y - 2, progressBarFill, 4);
 
     // Draw the border of the progress bar
-    drawContext.drawGuiTexture(progressBarBorderTexture, x, y - 2, 108, 4);
+    drawContext.drawGuiTexture(RenderLayer.getGuiTextured(), progressBarBorderTexture, x, y - 2, 108, 4);
     }
 
 
